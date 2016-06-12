9c8d35527b180bae1f5d5ad0e776e62a2ec69c6d
Jamie Dyer <jamie@kernowsoul.com>
2016-06-12 17:35:22 +0100

    Code clean up

--- lib/simpre/helper.rb
+++ lib/simpre/helper.rb
@@ -1,9 +1,8 @@
 module Simpre
   module Helper
-
     def decorate(item_or_collection)
       if item_or_collection.respond_to?(:map)
-        item_or_collection.map{ |item| decorate(item) }
+        item_or_collection.map { |item| decorate(item) }
       else
         item = item_or_collection
         decorator = "#{item.class}Presenter".constantize
@@ -11,6 +10,5 @@ module Simpre
         decorator.new(item, self)
       end
     end
-
   end
 end
